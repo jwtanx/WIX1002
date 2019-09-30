c453ed0f43c65e09bbc2881454b67411c30c55b9
User <User@acer>
2019-09-30 23:03:11 +0800

    NetBeans useful shortcuts for Java.

---
+++ src/shortcut.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class shortcut {
+/*
+
+Alt+Insert                              Generate code
+Ctrl+Shift+I                            Fix all class imports
+Alt+Shift+I                             Fix selected class's import
+Alt+Shift+F                             Format selection (Indent)
+Alt+Shift+Arrow Keys                    Shift lines left/right/up/down
+Ctrl+Shift+R                            Rectangular Selection (Toggle)
+Ctrl+Shift+Arrow Up/Arrow Down          Copy lines up/down
+Ctrl/Alt+F12                            Inspect members/hierarchy
+Ctrl+Shift+C/Ctrl+/                     Add/remove comment lines
+Ctrl+E                                  Delete current line
+
+Java Editor Code Templates
+Type shortcut and press Tab
+    
+psvm	public static void main
+En	Enumeration
+Ex	Exception
+Psf	public static final
+Psfb	public static final boolean
+Psfi	public static final int
+Psfs	public static final String
+St	String
+ab	abstract
+as	assert true
+br	break
+bo	boolean
+ca	catch
+cl	class
+cn	continue
+db	double
+df	default
+eq	equal
+ex	extends
+pu	public
+pr	private
+pe	protected
+th	throws
+fy	finally
+im	implements
+ie	interface
+tw	throw
+ir	import
+le	length
+sout	System.out.println()
+soutv	System.out.println("obj"+obj)
+wh	while
+vo	volatile
+iof	instanceof
+fi	final
+fl	float
+fa	false
+sh	short
+psf	private static final
+psfb	private static final boolean
+psfi	private static fianl int
+psfs	private static final String
+pst	printStackTrace()
+bcom	/**/
+      
+}
