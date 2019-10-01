4285c6cbab349d47be105f8e870001a990b64089
User <User@acer>
2019-10-01 16:34:20 +0800

    Updates @ 011019, 16:35

---
+++ src/Lab4/L4Q1.java
@@ -0,0 +1,15 @@
+/*
+ * Write a program that accepts an integer from user. Then, display its entire factors in
+increasing order.
+
+    eg. 
+ */
+package Lab4;
+
+/**
+ *
+ * @author User
+ */
+public class L4Q1 {
+    
+}
