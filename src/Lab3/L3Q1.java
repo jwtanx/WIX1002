db2482833429ba646ec273429dbddbbac66a2535
User <User@acer>
2019-10-04 20:26:03 +0800

    L3Q1 @ 041019, 20:25

--- src/Lab3/L3Q1.java
+++ src/Lab3/L3Q1.java
@@ -23,6 +23,45 @@ public class L3Q1 {
         Scanner s = new Scanner(System.in);
         
         System.out.print("Enter two integer number: ");
+        
+        int a = s.nextInt();
+        int b = s.nextInt();    // <-- b will be captered after a and a space
+        
+        System.out.print("Enter the operand: ");
+        char operand = s.next().charAt(0);
+        
+        switch(operand){
+            case '+':
+                System.out.printf("%d + %d = %d\n", a, b, (a+b));
+                break;
+                
+            case '-':
+                System.out.printf("%d - %d = %d\n", a, b, (a-b));
+                break;
+                
+            case '*':
+                System.out.printf("%d * %d = %d\n", a, b, (a*b));
+                break;
+                
+            case '/':
+                System.out.printf("%d / %d = %.2f\n", a, b, (a/(b*1.0)));
+                break;
+                
+            case '%':
+                System.out.printf("%d %% %d = %d\n", a, b, (a%b));
+                break;
+                
+            default:
+                System.out.println("Invalid operand! Console restarted");
+                main(args);
+                
+        }
+        System.out.println("\nDone on 30 Sept 2019, 9:40P.M.\nUpdated on 4 Oct 2019, 8:24P.M.");
+        /*
+===========
+OLD VERSION
+===========        
+        
         String number[] = s.nextLine().split(" ");
         
         int a = Integer.parseInt(number[0]);
@@ -73,5 +112,6 @@ public class L3Q1 {
             main(args);
         else
             System.out.println("Done on 30 Sept 2019, 9:40P.M.");
+        */
     }
 }
