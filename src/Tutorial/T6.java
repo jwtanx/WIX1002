9c1b4b92261d99df773d25d5a5b7e2e4182cb3e9
User <User@acer>
2019-10-20 21:57:04 +0800

    T6 Q1d - 1e @ 201019, 21:56

--- src/Tutorial/T6.java
+++ src/Tutorial/T6.java
@@ -32,6 +32,7 @@ public class T6 {
         for(int c = 0;  c < num; c++) {
             if(num == Math.pow(c, 2)) {
                 a = true;
+                break;
             }
         }
         
@@ -51,6 +52,36 @@ public class T6 {
 
         return (Nfact.divide((Kfact.multiply(NsubKfact))));
         
+    }
+    
+    //Q1D. Define a static method that used to determine whether the parameter is a pentagonal number. A pentagonal number is a figurate number that extends the concept of triangular and square numbers to the pentagon. Pn = ½ n(3n -1) 
+    static boolean checkPentagonal(int pnum){
+        boolean d = false;
+        int Pn;
+        
+        for(int n = 0; n < pnum; n++){
+            Pn = (n * ((3 * n) - 1)) / 2;
+            
+            if(Pn == pnum || pnum == 1) {
+                d = true;
+                break;
+            }
+        }
+        return d;
+    }
+    
+    //Q1E. Define a static method that displays the number of letters and the number of digits of a String parameter. 
+    static int[] numOfLetterNDigit(String sentence){
+        
+        int numOfLnD[] = new int[2];
+        
+        String numOfDigit = sentence.replaceAll("\\D+", "");
+        String numOfLetter = sentence.replaceAll("\\W+", "");
+        
+        numOfLnD[0] = numOfDigit.length();
+        numOfLnD[1] = numOfLetter.length() - numOfDigit.length();
+        
+        return numOfLnD;
     }
     
     
@@ -94,16 +125,30 @@ public class T6 {
                 
             case 'D':
             case 'd':
+                //Q1D
+                System.out.println("\nQ1D:");
+                System.out.print("Enter a num to check if it is pentagonal number: "); int pnum = s.nextInt();
                 
+                System.out.printf("Number %d is a pentagonal number: %b\n\n", pnum, checkPentagonal(pnum));
                 break;
                 
             case 'E':
             case 'e':
+                //Q1E
+                s.nextLine();
+                System.out.println("\nQ1E");
+                System.out.print("Type a sentence to check the number of digits & letters: "); String e = s.nextLine();
                 
+                int numOfLnD[] = numOfLetterNDigit(e);
+                System.out.printf("Number of digits: %d\n", numOfLnD[0]);
+                System.out.printf("Number of letters: %d\n\n", numOfLnD[1]);
                 break;
                 
             case 'F':
             case 'f':
+                
+                
+                
                 
                 break;
                 
