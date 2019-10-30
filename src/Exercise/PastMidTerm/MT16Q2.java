1bea8046870dae33f0afbfb3f70455005d522ade
User <User@acer>
2019-10-30 21:57:41 +0800

    MidTerm 2016 Q2 @ 301019, 22:00

---
+++ src/Exercise/PastMidTerm/MT16Q2.java
@@ -0,0 +1,44 @@
+/*
+ * 2. Write a Java program that reads integers input from the user. The program should display all the integers from 1 to 9 where the input ends with number -1. The program will display the minimum number and the occurrence count for the minimum number.
+ */
+package Exercise.PastMidTerm;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class MT16Q2 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Integer (-1 to quit): ");
+        int num = s.nextInt();
+        int min = num;
+        int count = 0;
+        
+        while(num != -1){
+            
+            num = s.nextInt();
+            
+            if(num == -1) break;
+            
+            else{
+                if(num > 0 && num <= 9){
+                    if(num < min){
+                        min = num;
+                        count = 1;
+                    }
+                    else if(num == min) count++;
+                    
+                    System.out.print(num + " ");
+                }
+            } 
+        }
+        System.out.println("\nThe minimun number is " + min);
+        System.out.println("The occurence count of " + min + " is " + count);
+        
+        System.out.println("\n\nDone @ 301019, 22:00");
+    }
+}
