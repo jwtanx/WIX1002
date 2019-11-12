8ce6a88647d27bec8c3f990d958456dc42a6ceb0
User <User@acer>
2019-11-13 00:24:41 +0800

    L7Q1 @ 131119, 00:21

--- src/Lab7/L7Q1.java
+++ src/Lab7/L7Q1.java
@@ -22,48 +22,47 @@ import java.util.Scanner;
  * @author User
  */
 public class L7Q1 {
+
     public static void main(String[] args) {
-        
+
         String[][] course = {{"WXES1116", "Programming I"},
-                             {"WXES1115", "Data Structure"},
-                             {"WXES1110", "Operating System"},
-                             {"WXES1112", "Computing Mathematics"}};
-        
-        try{
+        {"WXES1115", "Data Structure"},
+        {"WXES1110", "Operating System"},
+        {"WXES1112", "Computing Mathematics"}};
+
+        try {
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("C:/Users/User/Documents/NetBeansProjects/WIX1002/src/Lab7/Files/course.dat"));
-            
-            for(int i = 0; i < course.length; i++){
-                for(int j = 0; j < course[i].length; j++){
+
+            for (int i = 0; i < course.length; i++) {
+                for (int j = 0; j < course[i].length; j++) {
                     out.writeUTF(course[i][j]);
                 }
             }
-            
+
             out.close();
-            
+
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("C:/Users/User/Documents/NetBeansProjects/WIX1002/src/Lab7/Files/course.dat"));
-            
+
             Scanner s = new Scanner(System.in);
             System.out.print("Enter your course code\t: ");
             String code = s.nextLine();
             String temp;
-            
-            try{
-                while(true){
+
+            try {
+                while (true) {
                     temp = in.readUTF();
-                    if(code.equalsIgnoreCase(temp)){
+                    if (code.equalsIgnoreCase(temp)) {
                         System.out.println("Your course name\t: " + in.readUTF());
                         break;
                     }
                 }
-            } catch(EOFException EOF){
-                
+            } catch (EOFException EOF) {
+
             }
-            
-            
-        } catch(IOException IO){
+
+        } catch (IOException IO) {
             System.err.println("Problem with file output!");
         }
-        
-        
+
     }
 }
