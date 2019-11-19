e6990498bb451af6262fa1afb8fd891bea6a38b9
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-19 19:41:19 +0800

    T8Q1A-H Tester Class @ 191119, 19:42

---
+++ src/Tutorial/T8/StudentTesterClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tutorial.T8;
+
+/**
+ *
+ * @author User
+ */
+public class StudentTesterClass {
+    public static void main(String[] args) {
+        
+        // 1g. Create an object of the class Student.
+        Student a = new Student("012-3456789");
+        System.out.println(a.toString());
+        
+        // 1h. Change the contact number using the mutator method.
+        System.out.println("Change the contact number...");
+        a.setContactNumber("013-9990000");
+        System.out.println(a.toString());
+        
+        System.out.println("\nDone @ 191119 19:40");
+    }
+}
