4aa759f6f5a342d4f4832d54d007aac6d3126cad
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-19 19:41:58 +0800

    T8Q1A-H Class @ 191119, 19:42

--- src/Tutorial/T8/Student.java
+++ src/Tutorial/T8/Student.java
@@ -1,14 +1,56 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * 1. Write statements for each of the following
+
+a. Define a class Student.
+b. Declare the instance variable that used to store contact number.
+c. Create the constructor that initializes the contact number to null.
+d. Create another constructor that assign the parameter value to the contact number. 
+e. Create an accessor and mutator method for the contact number.
+f. Create a method that used to display the contact number.
+g. Create an object of the class Student.
+h. Change the contact number using the mutator method.
+i. Create an object of the class Animal.
+j. Create an object of the class Animal that used to represent a cat. 
+k. Create an object of the class Number with the value 20 and 40.
+*/
+
 package Tutorial.T8;
 
-/**
- *
- * @author User
- */
+// 1a. Define a class Student.
 public class Student {
+    
+    // 1b. Declare the instance variable that used to store contact number.
+    private String contactNumber;
+    
+    // c. Create the constructor that initializes the contact number to null.
+    public Student(){
+        contactNumber = null;
+    }
+    
+    // 1d. Create another constructor that assign the parameter value to the contact number.     
+    public Student(String contactNumber){
+        this.contactNumber = contactNumber;
+    }
+    
+    // 1e. Create an accessor and mutator method for the contact number.
+    // Accessor
+    public String getContactNumber(){
+        return contactNumber;
+    }
+    
+    // Mutator
+    public void setContactNumber(String contactNumber){
+        this.contactNumber = contactNumber;
+    }
+    
+    // 1f. Create a method that used to display the contact number.
+    public String toString(){
+        return "Contact number : " + contactNumber;
+    }
+    
+    
+    
+    
+    
     
 }
