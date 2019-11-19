84be19cbe51d5673f9efc8a281f310ee3476c925
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-19 19:42:56 +0800

    T8Q1A-H Class @ 191119, 19:45

--- src/Tutorial/T8/Student.java
+++ src/Tutorial/T8/Student.java
@@ -12,45 +12,39 @@ h. Change the contact number using the mutator method.
 i. Create an object of the class Animal.
 j. Create an object of the class Animal that used to represent a cat. 
 k. Create an object of the class Number with the value 20 and 40.
-*/
-
+ */
 package Tutorial.T8;
 
 // 1a. Define a class Student.
 public class Student {
-    
+
     // 1b. Declare the instance variable that used to store contact number.
     private String contactNumber;
-    
+
     // c. Create the constructor that initializes the contact number to null.
-    public Student(){
+    public Student() {
         contactNumber = null;
     }
-    
+
     // 1d. Create another constructor that assign the parameter value to the contact number.     
-    public Student(String contactNumber){
+    public Student(String contactNumber) {
         this.contactNumber = contactNumber;
     }
-    
+
     // 1e. Create an accessor and mutator method for the contact number.
     // Accessor
-    public String getContactNumber(){
+    public String getContactNumber() {
         return contactNumber;
     }
-    
+
     // Mutator
-    public void setContactNumber(String contactNumber){
+    public void setContactNumber(String contactNumber) {
         this.contactNumber = contactNumber;
     }
-    
+
     // 1f. Create a method that used to display the contact number.
-    public String toString(){
+    public String toString() {
         return "Contact number : " + contactNumber;
     }
-    
-    
-    
-    
-    
-    
+
 }
