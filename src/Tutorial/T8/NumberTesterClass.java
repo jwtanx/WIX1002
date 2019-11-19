70d31e15de3f3e203d7fbae8733d0e6c79e057aa
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-19 20:20:51 +0800

    T8Q1K Tester Class @ 191119, 20:20

---
+++ src/Tutorial/T8/NumberTesterClass.java
@@ -0,0 +1,20 @@
+/*
+ * 1k. Create an object of the class Number with the value 20 and 40.
+ */
+package Tutorial.T8;
+
+/**
+ *
+ * @author User
+ */
+public class NumberTesterClass {
+    public static void main(String[] args) {
+        
+        Number ab = new Number(20, 40);
+        
+        System.out.println(ab.toString());
+        
+        System.out.println("\nDone @ 191119, 20:20");
+        
+    }
+}
