56cd78ee2b2cfcbe6d7c3a5d961c61d919cff530
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-19 20:44:57 +0800

    T8Q2K Tester Class @ 191119, 20:45

---
+++ src/Tutorial/T8/DigitTesterClass.java
@@ -0,0 +1,15 @@
+/*
+ * 2f. Create a tester class that displays the digit multiplication of 4567.  
+ */
+package Tutorial.T8;
+
+// 2f. Create a tester class that displays the digit multiplication of 4567.  
+public class DigitTesterClass {
+    public static void main(String[] args) {
+        
+        Digit d = new Digit(4567);
+        System.out.println(d.toString());
+        
+        System.out.println("\nDone @ 191119, 20:43");
+    }
+}
