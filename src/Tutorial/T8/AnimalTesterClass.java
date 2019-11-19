9b569cdd3e52be380f1c616467877fa69099667b
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-19 20:15:08 +0800

    T8Q1I-J Tester Class @ 191119, 20:15

---
+++ src/Tutorial/T8/AnimalTesterClass.java
@@ -0,0 +1,19 @@
+/*
+1i. Create an object of the class Animal.
+1j. Create an object of the class Animal that used to represent a cat.
+ */
+package Tutorial.T8;
+
+public class AnimalTesterClass {
+    public static void main(String[] args) {
+        
+        // 1i. Create an object of the class Animal.
+        Animal bird = new Animal("Twitties");
+        System.out.println(bird.toString());
+        
+        // 1j. Create an object of the class Animal that used to represent a cat.
+        Animal cat = new Animal();
+        cat.setName("Tom");
+        System.out.println(cat.toString());
+    }
+}
