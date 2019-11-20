62fe490ab142082a23314f562f011dbfa5f665bc
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-21 07:32:29 +0800

    T8Q4 Tester Class more features @ 211119, 07:32

--- src/Tutorial/T8/PaymentTesterClass.java
+++ src/Tutorial/T8/PaymentTesterClass.java
@@ -15,18 +15,21 @@ public class PaymentTesterClass {
         Scanner s = new Scanner(System.in);
         Payment a = new Payment();
         
-        System.out.print("Which payment type you want to use(Cash, Cheque, Credit Card): ");
+        System.out.print("Which payment type you want to use(Cash, Cheque, Bank Card): ");
         String paymentType = s.nextLine().toLowerCase();
         
         
         if(paymentType.equalsIgnoreCase("cash") || paymentType.contains("cas")){
-            System.out.print("Inser cash: RM");
+            System.out.println("\nPayment method\t\t: Cash");
+            System.out.print("Insert cash\t\t: RM");
             a.setCash(s.nextInt());
+            s.nextLine();
             
             System.out.println(a.toStringCash());
         }
         
         else if(paymentType.equalsIgnoreCase("cheque") || paymentType.contains("ch")){
+            System.out.println("\nPayment method\t\t: Cheque");
             System.out.print("Insert amount\t\t: RM");
             a.setAmount(s.nextDouble());
             s.nextLine();
@@ -36,18 +39,20 @@ public class PaymentTesterClass {
             System.out.println(a.toStringCheque());
         }
         
-        else if(paymentType.equalsIgnoreCase("credit card") || paymentType.contains("cr") || paymentType.contains("car")){
+        else if(paymentType.equalsIgnoreCase("Bank card") || paymentType.contains("b") || paymentType.contains("car")){
+            System.out.println("\nPayment method\t\t: Bank Card");
             System.out.print("Insert amount\t\t: RM");
             a.setAmount(s.nextDouble());
             s.nextLine();
             System.out.print("Insert card holder name\t: ");
             a.setCardHolderName(s.nextLine());
-            System.out.print("Insert card type\t: ");
-            a.setCardType(s.nextLine());
+            System.out.print("Insert card type(D/C)\t: ");
+            a.setCardType(s.nextLine().charAt(0));
             System.out.print("Insert expiration date\t: ");
             a.setExpirationDate(s.nextLine());
             System.out.print("Insert validation code\t: ");
             a.setValidationCode(s.nextInt());
+            s.nextLine();
             
             System.out.println(a.toStringCard());
             
@@ -55,6 +60,13 @@ public class PaymentTesterClass {
         else{
             System.out.println("We dont accept kiss currency bro, its only applicable in Smosh world.");
         }
+        
+        System.out.print("Do you want to restart(Y/N)?");
+        char choice = s.nextLine().charAt(0);
+        if(choice == 'Y' || choice == 'y'){
+            PaymentTesterClass.main(args);
+        }else
+            
         
         System.out.println("\nDone @ 211119, 00:36");
     }
