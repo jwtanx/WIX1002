3b67ca1316dda39b792dc7fa0bfbe67e60957ff9
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-22 10:05:42 +0800

    L8Q1 Number Tester Class Add Objects @ 221119, 10:05

--- src/Lab8/NumberTesterClass.java
+++ src/Lab8/NumberTesterClass.java
@@ -11,6 +11,7 @@ public class NumberTesterClass {
     public static void main(String[] args) {
         
         Number a = new Number();
+        System.out.println("\nFor a:");
         System.out.println(a.toString());
         System.out.println(a.toStringEven());
         System.out.println(a.toStringMax());
@@ -18,6 +19,24 @@ public class NumberTesterClass {
         System.out.println(a.toStringAverage());
         System.out.println(a.toStringSqrt());
         
+        Number b = new Number(5);
+        System.out.println("\nFor b:");
+        System.out.println(b.toString());
+        System.out.println(b.toStringEven());
+        System.out.println(b.toStringMax());
+        System.out.println(b.toStringMin());
+        System.out.println(b.toStringAverage());
+        System.out.println(b.toStringSqrt());
         
+        Number c = new Number(4, 50);
+        System.out.println("\nFor c:");
+        System.out.println(c.toString());
+        System.out.println(c.toStringEven());
+        System.out.println(c.toStringMax());
+        System.out.println(c.toStringMin());
+        System.out.println(c.toStringAverage());
+        System.out.println(c.toStringSqrt());
+        
+        System.out.println("\nDone @ 221119, 10:04");
     }
 }
