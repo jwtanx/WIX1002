2c6e75b7c5261231b6bed6c8d88508fb89265636
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-28 18:21:44 +0800

    T9Q2 Tester Class @ 281119, 18:20

---
+++ src/Tutorial/T9/Q2TesterClass.java
@@ -0,0 +1,17 @@
+/*
+2. Define a class Organism. The class contains the initial size of the organism and the growth rate. Create a constructor to initialize the instance variables. Then, define a class Animal. Animal is an organism that has extra instance variable which is the amount of eating need. Create a constructor to initialize the instance variable and a method to display the Animal instance variables. 
+ */
+package Tutorial.T9;
+
+import java.util.Random;
+
+public class Q2TesterClass {
+    public static void main(String[] args) {
+        
+        Random r = new Random();
+        
+        Q2Animal a = new Q2Animal(1 + r.nextInt(100));
+        
+        System.out.println(a.toString());
+    }
+}
