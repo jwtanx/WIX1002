11fadb5bac22dde6eee8d08f7c7fb2b2765f6e91
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-28 08:31:07 +0800

    L9Q1 Shape @ 281119, 08:30

---
+++ src/Lab9/Shape.java
@@ -0,0 +1,41 @@
+
+package Lab9;
+
+import java.text.DecimalFormat;
+
+public class Shape {
+    
+    private String name;
+    protected double perimeter, area;
+    
+    public Shape(String name){
+        this.name = name;
+    }
+    
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setPerimeter(double perimeter) {
+        this.perimeter = perimeter;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+//    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("######.##");
+        return "\nName = " + name + "\nPerimeter = " + df.format(perimeter) + "\nArea = " + df.format(area);
+    }
+    
+    public void display(){
+        System.out.printf("Shape: %s\nPerimeter: %.2f\nArea: %.2f\n", name, perimeter, area); 
+    }
+    
+}
