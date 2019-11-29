d42e68c8df9229559b0f7a14e909b4a8f82dc26d
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-29 10:46:44 +0800

    L9Q1 Add Question @ 291119, 09:45

--- src/Lab9/Q1Shape.java
+++ src/Lab9/Q1Shape.java
@@ -1,4 +1,6 @@
-
+/*
+1. Define a class Shape. The class has a name, perimeter and area. The class has the accessor method and mutator for perimeter and area. Besides, the class also has a display method to display the name of the shape, perimeter and area in two decimal points. Derived a class name Rectangle from Shape. The class has the extra side length variables. The class has a method to accept input for both side length and a method to compute the perimeter and area.  Derived another class name Square from Shape.  The class has the extra side length variable. The class has a method to accept input for side length and a method to compute the perimeter and area.  Derived another class name Circle from Shape. The class has the extra diameter variable.    The class has a method to accept input for diameter and a method to compute the perimeter and area. Create a Tester class to test the program. 
+*/
 package Lab9;
 
 import java.text.DecimalFormat;
