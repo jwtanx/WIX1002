4b91218bb475fa68d6f690239bf74660467b9547
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-11-29 09:47:06 +0800

    L9Q1 Tester Class @ 291119, 09:45

---
+++ src/Lab9/Q1TesterClass.java
@@ -0,0 +1,13 @@
+
+package Lab9;
+
+public class Q1TesterClass {
+    public static void main(String[] args) {
+        
+        Q1Rectangle r = new Q1Rectangle();
+        r.input();
+        r.compute();
+        r.display();
+        
+    }
+}
