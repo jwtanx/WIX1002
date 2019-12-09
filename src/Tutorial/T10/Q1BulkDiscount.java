756f72e03ba6de4f38494c87789d99e8ab9fd550
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-12-09 09:35:34 +0800

    T10Q1 1st Child Class @ 091219, 09:35

---
+++ src/Tutorial/T10/Q1BulkDiscount.java
@@ -0,0 +1,27 @@
+/*
+1. cont. Derived a class BulkDiscount from DiscountPolicy. It has a constructor that has two parameters minimum and discount rate. It has a method computeDiscount that compute the discount base on discount rate if the number of item more than minimum. Otherwise, no discount given.
+ */
+package Tutorial.T10;
+
+public class Q1BulkDiscount extends Q1DiscountPolicy{
+    
+    private double minimum, discountRate;
+    
+    public Q1BulkDiscount(int minimum, double discountRate) {
+        this.minimum = minimum;
+        this.discountRate = discountRate;
+    }
+    
+    public double computeDiscount(int count, double itemCost){
+        
+        if(count >= minimum){
+            
+            discount += discountRate * itemCost;
+            return discountRate * itemCost;
+            
+        }
+        
+        return 0;
+    }
+    
+}
