6121788bc710d40b7e422c0dbd715090a5910f9e
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-12-09 17:32:42 +0800

    T10Q3 Interface class @ 091219, 17:32

---
+++ src/Tutorial/T10/Q3Comparable.java
@@ -0,0 +1,10 @@
+/*
+3. Create a class Person that implements the comparable interface. The class has an instance variable name. The class has the constructor that initializes the name. The class also has the accessor method and a display method to display the name. Create an array for multiple Person objects. Sort the person in ascending order. Create a Tester class to test the program.
+ */
+package Tutorial.T10;
+
+public interface Q3Comparable {
+    
+    public String[] compare(Object obj[]);
+    
+}
