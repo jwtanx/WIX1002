77c892360b9a3c57924bbfc97b20a99a38dd1637
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-12-09 10:13:31 +0800

    T10Q2 Interface @ 091219, 10:13

---
+++ src/Tutorial/T10/Q2Interest.java
@@ -0,0 +1,10 @@
+/*
+2. Create an interface Interest that has a single method computeInterest that return the monthly interest based on the balance in the account. Create the SavingAccount that implement the interface, the class has an instance variable called balance. Define the method to compute interest. The interest rate for saving account is 0.5% per year. Create the FixedAccount that implement the interface. The class has an instance variable called balance. Define the method to compute interest. The interest rate for saving account is 3% per year. Create a Tester class to test the program. 
+ */
+package Tutorial.T10;
+
+public interface Q2Interest {
+    
+    public double computeInterest();
+
+}
