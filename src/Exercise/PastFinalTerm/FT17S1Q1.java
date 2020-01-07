e39ac17cfbc69f4252a3d3e30a745a486191e082
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-01-07 12:19:18 +0800

    FT17S1Q1 added the number of the correction @ 070120, 12:18

--- src/Exercise/PastFinalTerm/FT17S1Q1.java
+++ src/Exercise/PastFinalTerm/FT17S1Q1.java
@@ -17,38 +17,38 @@ public class FT17S1Q1 {
         int number1 = 0, number2 = 0, number3 = 0, number4 = 0, number5 = 0;
         int inputNumber;
         int value = 0;
-        int counter = 1;
+        int counter = 1; // 1
 
         while (counter <= 5) {
             System.out.print("Enter number: ");
             inputNumber = input.nextInt();
 
             if (inputNumber >= 1 && inputNumber <= 30) {
-                switch (counter) {
+                switch (counter) { // 2
                     case 1:
                         number1 = inputNumber;
                         break;
                     case 2:
                         number2 = inputNumber;
                         break;
-                    case 3:
+                    case 3: // 3
                         number3 = inputNumber;
                         break;
-                    case 4:
+                    case 4: // 4
                         number4 = inputNumber;
                         break;
                     case 5:
                         number5 = inputNumber;
                         break;
-                }
-                counter++;
+                }// 5
+                counter++; //10
             } else {
                 System.out.println("Invalid Input\nNumber should be between 1 and 30");
             }
         }
 
-        for (counter = 1; counter <= 5; counter++) {
-            switch (counter) {
+        for (counter = 1; counter <= 5; counter++) { // 6
+            switch (counter) { // 7
                 case 1:
                     value = number1;
                     break;
@@ -65,7 +65,7 @@ public class FT17S1Q1 {
                     value = number5;
                     break;
             }
-            for (int j = 1; j <= value; j++) {
+            for (int j = 1; j <= value; j++) { // 8 & 9
                 System.out.print("*");
             }
                 System.out.println();
